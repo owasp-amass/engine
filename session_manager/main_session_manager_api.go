1240e19796d970a2b4b201b7762dc82b91b0ec1f
Paolo Fabio Zaino <p.zaino@zfpsystems.com>
2023-10-22 12:18:41 +0100

    Added exceptions handling

--- session_manager/main_session_manager_api.go
+++ session_manager/main_session_manager_api.go
@@ -1,6 +1,8 @@
 package session_manager
 
 import (
+	"fmt"
+
 	"github.com/google/uuid"
 )
 
@@ -27,27 +29,44 @@ func SessionManagerInit() {
 
 // SessionManagerShutdown cleans up the global session storage.
 func SessionManagerShutdown() {
+	if sessionStorage == nil {
+		return
+	}
 	sessionStorage.CleanAllSessions()
 	// reset the global session storage
+
 	sessionStorage = nil
 }
 
 // SessionManagerAddSession adds a session to the global session storage.
 func SessionManagerAddSession(s *SessionConfig) uuid.UUID {
+	if sessionStorage == nil {
+		return uuid.UUID{}
+	}
 	return sessionStorage.AddSession(s)
 }
 
 // SessionManagerCancelSession cancels a session in the global session storage.
-func SessionManagerCancelSession(id uuid.UUID) {
+func SessionManagerCancelSession(id uuid.UUID) error {
+	if sessionStorage == nil {
+		return fmt.Errorf("session manager is not initialized")
+	}
 	sessionStorage.CancelSession(id)
+	return nil
 }
 
 // SessionManagerGetSession gets a session from the global session storage.
 func SessionManagerGetSession(id uuid.UUID) *SessionConfig {
+	if sessionStorage == nil {
+		return nil
+	}
 	return sessionStorage.GetSession(id)
 }
 
 // SessionManagerCleanAllSessions cleans all sessions from the global session storage.
 func SessionManagerCleanAllSessions() {
+	if sessionStorage == nil {
+		return
+	}
 	sessionStorage.CleanAllSessions()
 }
