0c84504e7c6a7fdf2ab3fc2d50f9e497713b4fbc
caffix <caffix@users.noreply.github.com>
2024-02-19 16:43:55 -0500

    important fixes to the NamesToAddrs database queries

--- graph/addr.go
+++ graph/addr.go
@@ -41,10 +41,46 @@ func (g *Graph) NamesToAddrs(ctx context.Context, since time.Time, names ...stri
 	defer remaining.Close()
 	remaining.InsertMany(names...)
 
-	from := "(relations inner join assets on relations.from_asset_id = assets.id)"
-	where := " where assets.type = 'FQDN' and relations.type in ('a_record','aaaa_record')"
+	// get the IPs associated with SRV, NS, and MX records
+	sel := "SELECT srvs.content->>'name' AS name,ips.content->>'address' AS addr "
+	from := "FROM ((((assets AS fqdns INNER JOIN relations AS r1 ON fqdns.id = r1.from_asset_id) "
+	from2 := "INNER JOIN assets AS srvs ON r1.to_asset_id = srvs.id) INNER JOIN relations AS r2 ON srvs.id ="
+	from3 := " r2.from_asset_id) INNER JOIN assets AS ips ON r2.to_asset_id = ips.id)"
+	where := " WHERE fqdns.type = 'FQDN' AND srvs.type = 'FQDN' AND ips.type = 'IPAddress'"
+	where2 := " AND r1.type IN ('srv_record','ns_record','mx_record') AND r2.type IN ('a_record','aaaa_record')"
+	query := sel + from + from2 + from3 + where + where2
+	if !since.IsZero() {
+		query += " AND r1.last_seen > " + since.Format("2006-01-02 15:04:05") +
+			" AND r2.last_seen > " + since.Format("2006-01-02 15:04:05")
+	}
+	query += " AND fqdns.content->>'name' in ('" + strings.Join(remaining.Slice(), "','") + "')"
+
+	var results []struct {
+		Name string `gorm:"column:name"`
+		Addr string `gorm:"column:addr"`
+	}
+
+	if err := g.DB.RawQuery(query, &results); err == nil && len(results) > 0 {
+		for _, res := range results {
+			if !remaining.Has(res.Name) {
+				continue
+			}
+			remaining.Remove(res.Name)
+			if _, found := nameAddrMap[res.Name]; !found {
+				nameAddrMap[res.Name] = stringset.New()
+			}
+			nameAddrMap[res.Name].Insert(res.Addr)
+		}
+	}
+
+	if remaining.Len() == 0 {
+		return generatePairsFromAddrMap(nameAddrMap)
+	}
+
+	from = "(relations inner join assets on relations.from_asset_id = assets.id)"
+	where = " where assets.type = 'FQDN' and relations.type in ('a_record','aaaa_record')"
 	likeset := " and assets.content->>'name' in ('" + strings.Join(remaining.Slice(), "','") + "')"
-	query := from + where + likeset
+	query = from + where + likeset
 	if !since.IsZero() {
 		query += " and relations.last_seen > " + since.Format("2006-01-02 15:04:05")
 	}
@@ -62,39 +98,6 @@ func (g *Graph) NamesToAddrs(ctx context.Context, since time.Time, names ...stri
 				nameAddrMap[f.Name].Insert(a.Address.String())
 				remaining.Remove(f.Name)
 			}
-		}
-	}
-
-	if remaining.Len() == 0 {
-		return generatePairsFromAddrMap(nameAddrMap)
-	}
-
-	// get the IPs associated with SRV, NS, and MX records
-	sel := "SELECT fqdns.content->>'name',ips.content->>'address'"
-	from = "FROM ((((assets AS fqdns INNER JOIN relations AS r1 ON fqdns.id = r1.from_asset_id)"
-	from2 := "INNER JOIN assets AS srvs ON r1.to_asset_id = srvs.id) INNER JOIN relations AS r2 ON srvs.id ="
-	from3 := " r2.from_asset_id) INNER JOIN assets AS ips ON r2.to_asset_id = ips.id)"
-	where = " WHERE fqdns.type = 'FQDN' AND srvs.type = 'FQDN' AND ips.type = 'IPAddress'"
-	where2 := " AND r1.type IN ('srv_record','ns_record','mx_record') AND r2.type IN ('a_record','aaaa_record')"
-	query = sel + from + from2 + from3 + where + where2
-	if !since.IsZero() {
-		query += " AND r1.last_seen > " + since.Format("2006-01-02 15:04:05") +
-			" AND r2.last_seen > " + since.Format("2006-01-02 15:04:05")
-	}
-	query += " AND fqdns.content->>'name' in ('" + strings.Join(remaining.Slice(), "','") + "')"
-
-	var results []struct {
-		Name string `gorm:"column:fqdns.content->>'name'"`
-		Addr string `gorm:"column:ips.content->>'address'"`
-	}
-
-	if err := g.DB.RawQuery(query, &results); err == nil && len(results) > 0 {
-		for _, res := range results {
-			remaining.Remove(res.Name)
-			if _, found := nameAddrMap[res.Name]; !found {
-				nameAddrMap[res.Name] = stringset.New()
-			}
-			nameAddrMap[res.Name].Insert(res.Addr)
 		}
 	}
 
@@ -126,8 +129,8 @@ func (g *Graph) NamesToAddrs(ctx context.Context, since time.Time, names ...stri
 	// get to the end of the CNAME alias chains
 	for _, name := range cnames {
 		var results []struct {
-			Name string `gorm:"column:fqdns.content->>'name'"`
-			Addr string `gorm:"column:ips.content->>'address'"`
+			Name string `gorm:"column:name"`
+			Addr string `gorm:"column:addr"`
 		}
 
 		if err := g.DB.RawQuery(cnameQuery(name, since), &results); err == nil && len(results) > 0 {
@@ -155,7 +158,7 @@ func cnameQuery(name string, since time.Time) string {
 		query += " and relations.last_seen > " + since.Format("2006-01-02 15:04:05")
 	}
 	query += " AND relations.type = 'cname_record' AND fqdns.content->>'name' = traverse_cname.fqdn)"
-	query += " SELECT fqdns.content->>'name', ips.content->>'address'"
+	query += " SELECT fqdns.content->>'name' AS name, ips.content->>'address' AS addr"
 	query += " FROM ((assets AS fqdns INNER JOIN relations ON fqdns.id = relations.from_asset_id)"
 	query += " INNER JOIN assets AS ips ON relations.to_asset_id = ips.id)"
 	query += " WHERE fqdns.type = 'FQDN' AND ips.type = 'IPAddress'"
