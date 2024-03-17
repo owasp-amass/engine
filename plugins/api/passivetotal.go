631e07ecb82dbe0122199bd14c554ae2712f26b0
caffix <caffix@users.noreply.github.com>
2024-03-16 21:10:16 -0400

    initial commit

---
+++ plugins/api/passivetotal.go
@@ -0,0 +1,150 @@
+// Copyright © by Jeff Foley 2024. All rights reserved.
+// Use of this source code is governed by Apache 2 LICENSE that can be found in the LICENSE file.
+// SPDX-License-Identifier: Apache-2.0
+
+package api
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"log/slog"
+	"strings"
+
+	"github.com/owasp-amass/engine/net/dns"
+	"github.com/owasp-amass/engine/net/http"
+	"github.com/owasp-amass/engine/plugins/support"
+	et "github.com/owasp-amass/engine/types"
+	oam "github.com/owasp-amass/open-asset-model"
+	"github.com/owasp-amass/open-asset-model/domain"
+	"go.uber.org/ratelimit"
+)
+
+type passiveTotal struct {
+	Name   string
+	log    *slog.Logger
+	rlimit ratelimit.Limiter
+}
+
+func NewPassiveTotal() et.Plugin {
+	return &passiveTotal{
+		Name:   "PassiveTotal",
+		rlimit: ratelimit.New(10, ratelimit.WithoutSlack),
+	}
+}
+
+func (pt *passiveTotal) Start(r et.Registry) error {
+	pt.log = r.Log().WithGroup("plugin").With("name", pt.Name)
+
+	name := pt.Name + "-Handler"
+	if err := r.RegisterHandler(&et.Handler{
+		Name:       name,
+		Transforms: []string{"fqdn"},
+		EventType:  oam.FQDN,
+		Callback:   pt.check,
+	}); err != nil {
+		r.Log().Error(fmt.Sprintf("Failed to register a handler: %v", err),
+			slog.Group("plugin", "name", pt.Name, "handler", name))
+		return err
+	}
+
+	pt.log.Info("Plugin started")
+	return nil
+}
+
+func (pt *passiveTotal) Stop() {
+	pt.log.Info("Plugin stopped")
+}
+
+func (pt *passiveTotal) check(e *et.Event) error {
+	fqdn, ok := e.Asset.Asset.(*domain.FQDN)
+	if !ok {
+		return errors.New("failed to extract the FQDN asset")
+	}
+
+	ds := e.Session.Config().GetDataSourceConfig(pt.Name)
+	if ds == nil || len(ds.Creds) == 0 {
+		return nil
+	}
+
+	domlt := strings.ToLower(strings.TrimSpace(fqdn.Name))
+	if e.Session.Config().WhichDomain(domlt) != domlt {
+		return nil
+	}
+
+	matches, err := e.Session.Config().CheckTransformations("fqdn", "fqdn", "passivetotal")
+	if err != nil {
+		return err
+	}
+	if !matches.IsMatch("fqdn") {
+		return nil
+	}
+
+	for _, cr := range ds.Creds {
+		if cr == nil || cr.Username == "" || cr.Apikey == "" {
+			continue
+		}
+
+		var lastid string
+		if body, err := pt.query(domlt, "", cr.Username, cr.Apikey); err == nil {
+			lastid = pt.process(e, domlt, body)
+
+			for lastid != "" {
+				id := lastid
+				lastid = ""
+				if body, err := pt.query(domlt, id, cr.Username, cr.Apikey); err == nil {
+					lastid = pt.process(e, domlt, body)
+				}
+			}
+			break
+		}
+
+		e.Session.Log().Error(fmt.Sprintf("Failed to use the API endpoint: %v", err),
+			slog.Group("plugin", "name", pt.Name, "handler", pt.Name+"-Handler"))
+	}
+	return nil
+}
+
+func (pt *passiveTotal) query(domain, lastid, username, key string) (string, error) {
+	pt.rlimit.Take()
+
+	url := "https://api.riskiq.net/pt/v2/enrichment/subdomains?query=" + domain
+	if lastid != "" {
+		url += "&lastId=" + lastid
+	}
+
+	resp, err := http.RequestWebPage(context.TODO(), &http.Request{
+		URL: url,
+		Auth: &http.BasicAuth{
+			Username: username,
+			Password: key,
+		},
+	})
+	if err != nil {
+		return "", err
+	}
+	return resp.Body, nil
+}
+
+func (pt *passiveTotal) process(e *et.Event, domain, body string) string {
+	var result struct {
+		Success    bool     `json:"success"`
+		Subdomains []string `json:"subdomains"`
+		LastID     string   `json:"lastId"`
+	}
+
+	if err := json.Unmarshal([]byte(body), &result); err != nil || !result.Success {
+		return ""
+	}
+
+	for _, sub := range result.Subdomains {
+		fqdn := dns.RemoveAsteriskLabel(http.CleanName(sub + "." + domain))
+		// if the subdomain is not in scope, skip it
+		fmt.Println(fqdn)
+		if fqdn != "" && e.Session.Config().IsDomainInScope(fqdn) {
+			support.SubmitFQDNGuess(e, fqdn)
+		}
+	}
+	return result.LastID
+}
